5335ad0e0b896ae67fcfc8794cb4bec99e428452
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-04-07 19:28:47 +0200

    Added mocks to bank operations tests

--- src/test/java/pl/put/miasi/bank/bankOperations/bankProductOperations/BankProductOperationUtilTest.java
+++ src/test/java/pl/put/miasi/bank/bankOperations/bankProductOperations/BankProductOperationUtilTest.java
@@ -1,39 +1,50 @@
 package pl.put.miasi.bank.bankOperations.bankProductOperations;
 
+import static org.easymock.EasyMock.*;
+import org.easymock.EasyMockSupport;
 import org.junit.Before;
 import org.junit.Test;
-import pl.put.miasi.bank.Client;
 import pl.put.miasi.bank.bankMechanisms.InterestMechanism;
-import pl.put.miasi.bank.bankMechanisms.LinearInterestMechanism;
 import pl.put.miasi.bank.bankMechanisms.exception.InterestRateException;
-import pl.put.miasi.bank.bankProducts.BankAccount;
 import pl.put.miasi.bank.bankProducts.BankProduct;
 import pl.put.miasi.bank.bankProducts.exception.BalanceException;
 
-import static org.junit.Assert.*;
-
-public class BankProductOperationUtilTest {
+public class BankProductOperationUtilTest extends EasyMockSupport {
     private BankProduct bankProduct;
+    private InterestMechanism interestMechanism;
 
     @Before
     public void before() throws InterestRateException, BalanceException {
-        bankProduct = new BankAccount();
-        bankProduct.setInterestMechanism(new LinearInterestMechanism(0.5));
-        bankProduct.updateBalance(1000);
+        bankProduct = mock(BankProduct.class);
+        interestMechanism = mock(InterestMechanism.class);
     }
 
     @Test
     public void testCalculateInterest() throws InterestRateException, BalanceException {
-        BankProductOperationUtil.calculateInterest("Obliczenie odsetek", bankProduct);
+        double balance = 1000.0;
+        double interest = 100.0;
 
-        assertEquals(bankProduct.getBalance(), 1500, 0);
+        expect(interestMechanism.calculateInterest(eq(balance))).andReturn(interest);
+
+        expect(bankProduct.getInterestMechanism()).andReturn(interestMechanism);
+        expect(bankProduct.getBalance()).andReturn(balance);
+        bankProduct.updateBalance(eq(interest));
+        bankProduct.addBankOperation(isA(InterestCalculation.class));
+
+        replayAll();
+
+        BankProductOperationUtil.calculateInterest("Obliczenie odsetek", bankProduct);
+        verifyAll();
     }
 
     @Test
     public void testChangeInterestMechanism() throws Exception {
-        InterestMechanism differentInterestMechanism = new LinearInterestMechanism(0.20);
-        BankProductOperationUtil.changeInterestMechanism("Test", bankProduct, differentInterestMechanism);
-        assertEquals(bankProduct.getInterestMechanism(), differentInterestMechanism);
+        bankProduct.setInterestMechanism(interestMechanism);
+        bankProduct.addBankOperation(isA(InterestMechanismChange.class));
+        replayAll();
+
+        BankProductOperationUtil.changeInterestMechanism("Test", bankProduct, interestMechanism);
+        verifyAll();
     }
 
     @Test
