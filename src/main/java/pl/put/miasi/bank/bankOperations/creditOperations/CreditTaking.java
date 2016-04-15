55090c04860f849a48da268e44e53ba955d476ca
gracz21 <kamil.walek21@gmail.com>
2016-04-15 11:13:21 +0200

    Naprawiono (prawie) operacje na kredytach

--- src/main/java/pl/put/miasi/bank/bankOperations/creditOperations/CreditTaking.java
+++ src/main/java/pl/put/miasi/bank/bankOperations/creditOperations/CreditTaking.java
@@ -1,12 +1,39 @@
 package pl.put.miasi.bank.bankOperations.creditOperations;
 
+import pl.put.miasi.bank.bankMechanisms.InterestMechanism;
+import pl.put.miasi.bank.bankOperations.BankOperation;
+import pl.put.miasi.bank.bankProducts.BankAccount;
 import pl.put.miasi.bank.bankProducts.Credit;
+
+import java.security.InvalidParameterException;
 
 /**
  * Wziecie kredyty
  */
-public class CreditTaking extends CreditOperation {
-    protected CreditTaking(String description, Credit credit) {
-        super(description, credit);
+public class CreditTaking extends BankOperation {
+    private double amount;
+    private BankAccount bankAccount;
+    private InterestMechanism interestMechanism;
+
+    public CreditTaking(String description, double amount, BankAccount bankAccount, InterestMechanism interestMechanism) {
+        super(description);
+        this.amount = amount;
+        this.bankAccount = bankAccount;
+        this.interestMechanism = interestMechanism;
+    }
+
+    @Override
+    public String getOperationName() {
+        return null;
+    }
+
+    @Override
+    public void execute() throws Exception {
+        if(this.amount <= 0) {
+            throw new InvalidParameterException("Credit amount is negative");
+        }
+        Credit credit = new Credit(amount);
+        bankAccount.updateBalance(amount);
+        credit.setInterestMechanism(interestMechanism);
     }
 }
