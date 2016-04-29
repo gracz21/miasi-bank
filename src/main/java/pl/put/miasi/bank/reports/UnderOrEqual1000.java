1899205098c5472805288330956b48fa635d4661
Szymon Soppa <sz.soppa@gmail.com>
2016-04-29 10:37:15 +0200

    Dodano raport UnderOrEqual1000

--- src/main/java/pl/put/miasi/bank/reports/UnderOrEqual1000.java
+++ src/main/java/pl/put/miasi/bank/reports/UnderOrEqual1000.java
@@ -6,21 +6,26 @@ import pl.put.miasi.bank.bankProducts.Deposit;
 import pl.put.miasi.bank.bankProducts.bankAccount.BankAccount;
 
 /**
- * Created by inf109714 on 2016-04-29.
+ * Created by inf109760 on 2016-04-29.
  */
-public class Under1000Report implements Report {
+public class UnderOrEqual1000 implements Report {
+    private int limit = 1000;
+
     @Override
     public BankProduct visit(BankAccount bankAccount) {
+        if (bankAccount.getBalance() <= 1000) return bankAccount;
         return null;
     }
 
     @Override
     public BankProduct visit(Credit credit) {
+        if (credit.isActive() && (credit.getBalance() <= 1000)) return credit;
         return null;
     }
 
     @Override
     public BankProduct visit(Deposit deposit) {
+        if (deposit.isActive() && deposit.getBalance() <= 1000) return deposit;
         return null;
     }
 }
